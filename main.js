e7812760467c2d3ca228dc380a01fb41846affb0
jakeoncob <161487209+jakeoncob@users.noreply.github.com>
2024-02-29 09:39:15 -0600

    Add files via upload
    create a fine tuned chatgpt-3.5-turbo-1106 using the onlyHTML.jsonl package and place it into const MODELEASY.

---
+++ main.js
@@ -0,0 +1,75 @@
+
+import { ALL } from 'dns';
+import { link } from 'fs';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+import { OpenAI } from "openai";
+require('dotenv').config()
+
+import { setTimeout } from 'timers/promises';
+const openai = new OpenAI({
+    apiKey: process.env.apiKey,
+    
+})
+const express = require("express")
+const app = express()
+const path = require('path')
+const cheerio = require('cheerio')
+const http = require('http')
+const jsdom = require('jsdom')
+const MODEL = 'gpt-4'
+const MODELEASY = 'ft:gpt-3.5-turbo-1106:personal:onlyhtmltrytwo:8xJoJRV2'
+const SYSTEMONE = "The assistant generates complete HTML, CSS, and JavaScript code for simple web-based games, providing the full code necessary to play the game without additional explanations (remember, no explanations, only whats inside the <html> brackets), black background and make it visually appealing aswell as preferably centered .(dont add anything like Below is a simple implementation of _____, just pure html code)(also remember to not invole the ```html start and ``` finishing things)"
+const SYSTEMTWO = "ChatGPT responds exclusively in HTML code."
+const ENDPOINT = 'https://api.openai.com/v1/chat/completions'
+const hostOne = 'localhost'
+const portOne = 8000
+const listGPT = []
+const fs = require('fs/promises')
+const bodyParser = require('body-parser')
+
+
+
+async function callGPT(var1,modelchange) {
+    var systemforGPT = ''
+    if (modelchange === MODEL) {
+        systemforGPT = SYSTEMONE
+    }
+    if (modelchange === MODELEASY) {
+        systemforGPT = SYSTEMTWO
+    }
+    const completion = await openai.chat.completions.create({
+        model: modelchange,
+        messages: [
+            {'role':'system','content':`${systemforGPT}`},
+            {'role':"user",'content':`${var1}`}
+        ]
+    })
+    listGPT.push(completion.choices[0].message.content)
+    
+    //console.log(listGPT[listGPT.length - 1])
+}
+
+app.get('/', (req, res) => {
+    
+    res.sendFile(path.join('/Users/jakecoble/Desktop/BaseForGPT/index.html'));
+});
+app.use(bodyParser.urlencoded({extended:true}))
+app.post('/submit', async (req,res) => {
+    const userInput = req.body.userInput;
+    const modelget = req.body.promptType
+    var themodel = ''
+    if (modelget === 'simple') {
+        themodel = MODELEASY
+    }
+    if (modelget === 'complex') {
+        themodel = MODEL
+    }
+    
+    await callGPT(userInput,themodel);
+    res.send(`${listGPT[listGPT.length - 1]}`)
+})
+
+app.listen(portOne, () => {
+    console.log(`Server is running on http://${hostOne}:${portOne}`)
+})
