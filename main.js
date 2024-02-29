a11ee43f5b913127360a6d29d4faf243ddd1f577
jakeoncob <161487209+jakeoncob@users.noreply.github.com>
2024-02-29 09:42:59 -0600

    main.js

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ import { link } from 'fs';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 import { OpenAI } from "openai";
-require('dotenv').config()
+require('dotenv').config() //create a process.env for api key if you feel like it if not just delete this
 
 import { setTimeout } from 'timers/promises';
 const openai = new OpenAI({
-    apiKey: process.env.apiKey,
+    apiKey: '', //your api key here
     
 })
 const express = require("express")
 const app = express()
 const path = require('path')
-const cheerio = require('cheerio')
+
 const http = require('http')
-const jsdom = require('jsdom')
+
 const MODEL = 'gpt-4'
 const MODELEASY = 'ft:gpt-3.5-turbo-1106:personal:onlyhtmltrytwo:8xJoJRV2'
 const SYSTEMONE = "The assistant generates complete HTML, CSS, and JavaScript code for simple web-based games, providing the full code necessary to play the game without additional explanations (remember, no explanations, only whats inside the <html> brackets), black background and make it visually appealing aswell as preferably centered .(dont add anything like Below is a simple implementation of _____, just pure html code)(also remember to not invole the ```html start and ``` finishing things)"
@@ -52,7 +52,7 @@ async function callGPT(var1,modelchange) {
 
 app.get('/', (req, res) => {
     
-    res.sendFile(path.join('/Users/jakecoble/Desktop/BaseForGPT/index.html'));
+    res.sendFile(path.join('/Users/jakecoble/Desktop/BaseForGPT/index.html')); // your path for index.html here
 });
 app.use(bodyParser.urlencoded({extended:true}))
 app.post('/submit', async (req,res) => {
